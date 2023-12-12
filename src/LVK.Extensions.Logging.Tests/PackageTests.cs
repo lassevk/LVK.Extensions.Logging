4ba1dbff28de7a91f0f03509c5924fd61315c4ef
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2023-12-12 09:01:51 +0100

    Nuget package tests

---
+++ src/LVK.Extensions.Logging.Tests/PackageTests.cs
@@ -0,0 +1,10 @@
+using LVK.Tests.Nuget;
+
+namespace LVK.Extensions.Logging.Tests;
+
+[TestFixture]
+[NugetProject("../../src/LVK.Extensions.Logging/LVK.Extensions.Logging.csproj")]
+public class PackageTests : NugetTests<PackageTests>
+{
+
+}
